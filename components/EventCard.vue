a165946122a4296f01c9f6e253f2c0139cf6b05c
j-grosse <103175813+j-grosse@users.noreply.github.com>
2024-04-26 08:51:15 +0200

    feat: fetch and show event data

---
+++ components/EventCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <NuxtLink :to="`/e/${event.permalink}`">
+        <img :src="getPrincipalEventImage(event)" alt="event image" />
+        <h2>{{ event.title }}</h2>
+        <p>{{ event.subtitle }}</p>
+        <p>Dauer: {{ event.default_duration }}</p>
+        <p>Bewertung: {{ event.rating }}</p>
+        <p>Preis: {{ event.default_price.formatted }}</p>
+      </NuxtLink>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      event: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      getPrincipalEventImage(event) {
+        const principalImage = event.images.find((image) => image.isPrincipal);
+        return principalImage ? principalImage.thumb : '';
+      },
+    },
+  };
+  </script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Top Erlebnisse in Deiner Nähe</h2>
+    <div v-for="event in events" :key="event.id">
+      <!-- Event card component -->
+      <EventCard :event="event" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const events = ref([])
+
+    onMounted(async () => {
+      // import event data
+      const eventData = require('@/data/events.json')
+      events.value = eventData
+    })
+
+    return {
+      events,
+    }
+  },
+}
+</script>
