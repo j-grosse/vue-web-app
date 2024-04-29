e50f6c0311283393f0b4e5c03098c62131f1fdfc
j-grosse <103175813+j-grosse@users.noreply.github.com>
2024-04-29 17:33:26 +0200

    style: event card styling

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="card text-center">
-    <NuxtLink :to="`/events/${event.permalink}`">
-      <img
-        :src="getPrincipalEventImage(event)"
-        class="thumb rounded-md"
-        alt="event image"
-      />
-      <div class="font-medium my-4">{{ event.title }}</div>
-      <!-- <Icon icon="mdi-light:home" /> -->
-      <p>Dauer: {{ event.default_duration }} Bewertung: {{ event.rating }}</p>
-      <p>Preis: {{ event.default_price.formatted }}</p>
-    </NuxtLink>
+  <div class="container">
+    <div
+      class="text-center word-break-word rounded-lg bg-white shadow-lg mx-auto m-4 p-3 text-xs sm:text-sm xl:text-lg"
+    >
+      <NuxtLink :to="`/events/${event.permalink}`">
+        <img
+          :src="getPrincipalEventImage(event)"
+          class="m-0 auto rounded-md"
+          alt="event image"
+        />
+        <div class="font-medium my-4">{{ event.title }}</div>
+        <div class="text-left mx-2">
+          <p>Dauer: {{ event.default_duration / 60 }} h</p>
+          <p>Rating: {{ event.rating }}</p>
+          <p>Preis: {{ event.default_price.formatted }}</p>
+        </div>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
@@ -30,8 +36,4 @@ const getPrincipalEventImage = (event) => {
 }
 </script>
 
-<style scoped>
-.thumb {
-  margin: 0 auto;
-}
-</style>
+<style scoped></style>
