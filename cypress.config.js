e23652bbd9ea35520bbf84dc38783eb6499acd71
j-grosse <103175813+j-grosse@users.noreply.github.com>
2024-05-01 08:16:41 +0200

    chore: update postcss

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   fixturesFolder: "cypress/fixtures",
   screenshotsFolder: "cypress/screenshots",
   videosFolder: "cypress/videos",
+  video: false, // Disable video recording
 
   e2e: {
     setupNodeEvents(on, config) {
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,11 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  ssr: true,
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/styles/main.css'],
+  // using Tailwind: '~/assets/css/tailwind.css'
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -36,5 +39,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-
 }
